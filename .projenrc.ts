837a2595044992e46db61786ee13040de9e0617c
Dave Shepherd <dave.shepherd@unibuddy.com>
2025-02-18 09:14:12 +0000

    chore(build): :construction_worker: enable automerge configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,34 +1,35 @@
-import { CollectionKind, PrimitiveType } from '@jsii/spec';
-import { ProjenStruct, Struct } from '@mrgrain/jsii-struct-builder';
-import { github } from 'projen';
-import { JsiiProject } from './src/projects/jsii';
+import { CollectionKind, PrimitiveType } from "@jsii/spec";
+import { ProjenStruct, Struct } from "@mrgrain/jsii-struct-builder";
+import { github } from "projen";
+import { JsiiProject } from "./src/projects/jsii";
 
 const project = new JsiiProject({
-  author: 'Dave Shepherd',
-  authorAddress: 'dave.shepherd@endor.me.uk',
-  codeOwners: ['sabre'],
-  defaultReleaseBranch: 'main',
-  description: 'A collection of projen modules',
-  devDeps: ['@mrgrain/jsii-struct-builder', 'constructs', 'projen'],
-  gitignore: ['.npmrc', '.vscode'],
+  author: "Dave Shepherd",
+  authorAddress: "dave.shepherd@endor.me.uk",
+  codeOwners: ["sabre"],
+  defaultReleaseBranch: "main",
+  description: "A collection of projen modules",
+  devDeps: ["@mrgrain/jsii-struct-builder", "constructs", "projen"],
+  gitignore: [".npmrc", ".vscode"],
   githubOptions: {
     projenCredentials: github.GithubCredentials.fromApp({}),
   },
-  name: 'projen-modules',
-  peerDeps: ['constructs', 'projen'],
+  autoMerge: true,
+  name: "projen-modules",
+  peerDeps: ["constructs", "projen"],
   projenrcTs: true,
   publishToPypi: {
-    distName: 'projen_modules',
-    module: 'projen_modules',
+    distName: "projen_modules",
+    module: "projen_modules",
   },
   readme: {
     description:
-      'A collection of custom projen modules, that can be used to bootstrap and maintain consistent project configuration, tooling, dependencies, and builds.',
+      "A collection of custom projen modules, that can be used to bootstrap and maintain consistent project configuration, tooling, dependencies, and builds.",
   },
-  repositoryUrl: 'https://github.com/daveshepherd/projen-modules.git',
+  repositoryUrl: "https://github.com/daveshepherd/projen-modules.git",
 });
 project.readme?.addSection(
-  'Creating a New Project',
+  "Creating a New Project",
   `
 \`\`\`
 npx projen new {project} --from projen-modules
@@ -42,36 +43,36 @@ Some projects may have required fields that need to be specified as part of this
 | --------------------------------------- | ------------------------- |
 | [npm-package](API.md#npmpackage-)       | A typescript npm package  |
 | [python-package](API.md#pythonpackage-) | A python package          |
-| [jsii-package](API.md#jsiiproject-)     | A typescript JSII package |`,
+| [jsii-package](API.md#jsiiproject-)     | A typescript JSII package |`
 );
 project.readme?.addSection(
-  'Project Structure',
+  "Project Structure",
   `All source is located in \`src\` and is grouped by:
 * \`components\` - these are common building blocks that can be used by projects to implement specific project functionality.
 * \`projects\` - these are projects that can be built from this project (see #something)
 * \`utils\` - these are helper functions that are often reused
 
-\`test\` contains tests, and mirrors the \`src\` directory structure. Within here there are \`__snapshots__\` which contain snapshots of project tests (see #section).`,
+\`test\` contains tests, and mirrors the \`src\` directory structure. Within here there are \`__snapshots__\` which contain snapshots of project tests (see #section).`
 );
 new ProjenStruct(project, {
-  name: 'JsiiProjectOptions',
-  filePath: 'src/projects/jsii/jsii-project-options.ts',
+  name: "JsiiProjectOptions",
+  filePath: "src/projects/jsii/jsii-project-options.ts",
 })
-  .mixin(Struct.fromFqn('projen.cdk.JsiiProjectOptions'))
-  .update('defaultReleaseBranch', { optional: true })
-  .replace('readme', {
+  .mixin(Struct.fromFqn("projen.cdk.JsiiProjectOptions"))
+  .update("defaultReleaseBranch", { optional: true })
+  .replace("readme", {
     docs: {
-      summary: 'Configuration of the README.md file',
+      summary: "Configuration of the README.md file",
     },
-    name: 'readme',
+    name: "readme",
     optional: true,
-    type: { fqn: 'projen-modules.ReadmeOptions' },
+    type: { fqn: "projen-modules.ReadmeOptions" },
   })
   .add({
     docs: {
-      summary: 'List of teams used to generate the CODEOWNERS file',
+      summary: "List of teams used to generate the CODEOWNERS file",
     },
-    name: 'codeOwners',
+    name: "codeOwners",
     type: {
       collection: {
         kind: CollectionKind.Array,
@@ -82,24 +83,24 @@ new ProjenStruct(project, {
     },
   });
 new ProjenStruct(project, {
-  name: 'NpmPackageOptions',
-  filePath: 'src/projects/npm/npm-package-options.ts',
+  name: "NpmPackageOptions",
+  filePath: "src/projects/npm/npm-package-options.ts",
 })
-  .mixin(Struct.fromFqn('projen.typescript.TypeScriptProjectOptions'))
-  .update('defaultReleaseBranch', { optional: true })
-  .replace('readme', {
+  .mixin(Struct.fromFqn("projen.typescript.TypeScriptProjectOptions"))
+  .update("defaultReleaseBranch", { optional: true })
+  .replace("readme", {
     docs: {
-      summary: 'Configuration of the README.md file',
+      summary: "Configuration of the README.md file",
     },
-    name: 'readme',
+    name: "readme",
     optional: true,
-    type: { fqn: 'projen-modules.ReadmeOptions' },
+    type: { fqn: "projen-modules.ReadmeOptions" },
   })
   .add({
     docs: {
-      summary: 'List of teams used to generate the CODEOWNERS file',
+      summary: "List of teams used to generate the CODEOWNERS file",
     },
-    name: 'codeOwners',
+    name: "codeOwners",
     type: {
       collection: {
         kind: CollectionKind.Array,
@@ -110,23 +111,23 @@ new ProjenStruct(project, {
     },
   });
 new ProjenStruct(project, {
-  name: 'PythonPackageOptions',
-  filePath: 'src/projects/python/python-package-options.ts',
+  name: "PythonPackageOptions",
+  filePath: "src/projects/python/python-package-options.ts",
 })
-  .mixin(Struct.fromFqn('projen.python.PythonProjectOptions'))
-  .replace('readme', {
+  .mixin(Struct.fromFqn("projen.python.PythonProjectOptions"))
+  .replace("readme", {
     docs: {
-      summary: 'Configuration of the README.md file',
+      summary: "Configuration of the README.md file",
     },
-    name: 'readme',
+    name: "readme",
     optional: true,
-    type: { fqn: 'projen-modules.ReadmeOptions' },
+    type: { fqn: "projen-modules.ReadmeOptions" },
   })
   .add({
     docs: {
-      summary: 'List of teams used to generate the CODEOWNERS file',
+      summary: "List of teams used to generate the CODEOWNERS file",
     },
-    name: 'codeOwners',
+    name: "codeOwners",
     type: {
       collection: {
         kind: CollectionKind.Array,
@@ -138,10 +139,10 @@ new ProjenStruct(project, {
   })
   .add({
     docs: {
-      default: 'true',
-      summary: 'Include a GitHub pull request template.',
+      default: "true",
+      summary: "Include a GitHub pull request template.",
     },
-    name: 'pullRequestTemplate',
+    name: "pullRequestTemplate",
     optional: true,
     type: {
       primitive: PrimitiveType.Boolean,
@@ -149,10 +150,10 @@ new ProjenStruct(project, {
   })
   .add({
     docs: {
-      default: 'default content',
-      summary: 'The contents of the pull request template.',
+      default: "default content",
+      summary: "The contents of the pull request template.",
     },
-    name: 'pullRequestTemplateContents',
+    name: "pullRequestTemplateContents",
     optional: true,
     type: {
       collection: {
