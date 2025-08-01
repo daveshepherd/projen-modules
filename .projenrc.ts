b8ba041116725b26bf93c7f35386e0c7a4645713
Dave Shepherd <dave.shepherd@endor.me.uk>
2025-08-01 11:59:52 +0100

    fix(deps): :arrow_up: Allow new minor projen versions

--- .projenrc.ts
+++ .projenrc.ts
@@ -16,7 +16,7 @@ const project = new JsiiProject({
   },
   majorVersion: 1,
   name: 'projen-modules',
-  peerDeps: ['constructs', 'projen@^0.92.0'],
+  peerDeps: ['constructs', 'projen@0.* >=0.92.0'],
   projenrcTs: true,
   publishToPypi: {
     distName: 'projen_modules',
